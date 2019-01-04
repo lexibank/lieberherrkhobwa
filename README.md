38653230b6852be1e252fc29e1d459d301f1ee48
chrzyki <christoph@functor-argument.net>
2019-01-04 13:38:00 +0100

    Simple and naive mapping without cognacy and manual fixes for the raw data. This needs to be reverted after clarification.

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # lieberherrkhobwa
+
+Cite the source dataset as
+
+> 
+
+## Statistics
+
+
+
+![Glottolog: 100%](https://img.shields.io/badge/Glottolog-100%25-brightgreen.svg "Glottolog: 100%")
+![Concepticon: 0%](https://img.shields.io/badge/Concepticon-0%25-red.svg "Concepticon: 0%")
+![Source: 0%](https://img.shields.io/badge/Source-0%25-red.svg "Source: 0%")
+
+- **Varieties:** 29
+- **Concepts:** 100
+- **Lexemes:** 2,990
+- **Synonymy:** 1.03
+- **Cognacy:** 0 cognates in 0 cognate sets
+- **Invalid lexemes:** 0
+- **Tokens:** 0
+- **Segments:** 0 (0 BIPA errors, 0 CTLS sound class errors, 0 CLTS modified)
+- **Inventory size (avg):** 0.00
+
+## Possible Improvements:
+
+
+
+- Entries missing sources: 2990/2990 (100.00%)
--- lexibank_lieberherrkhobwa.py
+++ lexibank_lieberherrkhobwa.py
@@ -1,8 +1,11 @@
-# coding=utf-8
 from __future__ import unicode_literals, print_function
 
+from clldutils.misc import slug
 from clldutils.path import Path
+from csvw.dsv import iterrows
 from pylexibank.dataset import Dataset as BaseDataset
+
+from language_mappings import MAPPINGS
 
 
 class Dataset(BaseDataset):
@@ -10,24 +13,34 @@ class Dataset(BaseDataset):
     dir = Path(__file__).parent
 
     def cmd_download(self, **kw):
-        """
-        Download files to the raw/ directory. You can use helpers methods of `self.raw`, e.g.
-
-        >>> self.raw.download(url, fname)
-        """
         pass
 
     def cmd_install(self, **kw):
-        """
-        Convert the raw data to a CLDF dataset.
+        data = self.raw / 'dataset_khobwa.csv'
 
-        Use the methods of `pylexibank.cldf.Dataset` after instantiating one as context:
-
-        >>> with self.cldf as ds:
-        ...     ds.add_sources(...)
-        ...     ds.add_language(...)
-        ...     ds.add_concept(...)
-        ...     ds.add_lexemes(...)
-        """
         with self.cldf as ds:
-            pass
+            for k, v in MAPPINGS.items():
+                ds.add_language(ID=k, Name=k, Glottocode=v)
+
+            language_index = {}
+            meaning = None
+
+            for i, row in enumerate(iterrows(data)):
+                if i == 0:
+                    for j, col in enumerate(row):
+                        if (j > 1) and col:
+                            language_index[j] = slug(col)
+                elif i > 0:
+                    if row[0].isdigit():
+                        for j, col in enumerate(row):
+                            if j == 1:
+                                meaning = '{0}_l{1}'.format(slug(col), i + 1)
+                                ds.add_concept(ID=meaning, Name=col)
+                            elif j > 1:
+                                ds.add_lexemes(
+                                    Value=col,
+                                    Language_ID=language_index[j],
+                                    Parameter_ID=meaning
+                                )
+                    elif row[0].isdigit() is False:
+                        print('Cognate Group')
