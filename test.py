8a557dd8e98899b1e6dddbde232da9fbb60a7383
xrotwang <xrotwang@googlemail.com>
2019-01-29 11:03:29 +0100

    add metadata for zenodo

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
 # coding: utf-8
 from __future__ import unicode_literals
 
+from clldutils.jsonlib import load
+from clldutils.path import Path
+
+
+def test_zenodo_json():
+    load(Path(__file__).parent / '.zenodo.json')
+
 
 def test_valid(cldf_dataset, cldf_logger):
     assert cldf_dataset.validate(log=cldf_logger)
